bc43b5275100a7a872f2051b10038277cfa644ae
MagicConch <350043743@qq.com>
2016-12-18 16:37:03 +0800

    childish edition : waiting for reconfiguration

--- Carousel/carousel.js
+++ Carousel/carousel.js
@@ -3,43 +3,193 @@ var videoGroup2 = ["SVD","FAMAS","HK416","FNC"];
 var videoGroup3 = ["NTW-20","SV-98","蝎式","UMP45"];
 var videoGroup4 = ["VECTOR","WA2000","春田步枪","李·英菲尔德"];
 
-var videoPicEle = $(".video-pic");
-var videoTitEle = $(".video-title");
+var $videoPicEle = $(".video-pic");
+var $videoTitEle = $(".video-title");
+
 
 $("#head").bind("click",function(event){
-  switch(event.attr("id")){
+  switch(event.target.getAttribute("id")){
     case "page-1":
     {
 
-      for(i = 0; i < videoPicEle.length;i++){
-        videoPicEle[i].children().attr("src","./images/" + videoGroup1[i] + ".jpg");
-        videoTitEle[i].children().text(videoGroup1[i]);
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup1[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup1[i]);
+        $(".selected").removeClass("selected");
+        $(event.target).addClass("selected");
       }
     }
     break;
     case "page-2":
     {
-      for(i = 0; i < videoPicEle.length;i++){
-        videoPicEle[i].children().attr("src","./images/" + videoGroup2[i] + ".jpg");
-        videoTitEle[i].children().text(videoGroup2[i]);
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup2[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup2[i]);
+        $(".selected").removeClass("selected");
+        $(event.target).addClass("selected");
       }
     }
     break;
     case "page-3":
     {
-      for(i = 0; i < videoPicEle.length;i++){
-        videoPicEle[i].children().attr("src","./images/" + videoGroup3[i] + ".jpg");
-        videoTitEle[i].children().text(videoGroup3[i]);
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup3[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup3[i]);
+        $(".selected").removeClass("selected");
+        $(event.target).addClass("selected");
       }
     }
     break;
     case "page-4":
     {
-      for(i = 0; i < videoPicEle.length;i++){
-        videoPicEle[i].children().attr("src","./images/" + videoGroup4[i] + ".jpg");
-        videoTitEle[i].children().text(videoGroup4[i]);
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup4[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup4[i]);
+        $(".selected").removeClass("selected");
+        $(event.target).addClass("selected");
+      }
+    }
+    break;
+    case "left-btn":
+    {
+      switch($(".selected").attr("id")){
+        case "page-1":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup4[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup4[i]);
+            $(".selected").removeClass("selected");
+            $("#page-4").addClass("selected");
+          }
+        }
+        break;
+        case "page-2":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup1[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup1[i]);
+            $(".selected").removeClass("selected");
+            $("#page-1").addClass("selected");
+          }
+        }
+        break;
+        case "page-3":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup2[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup2[i]);
+            $(".selected").removeClass("selected");
+            $("#page-2").addClass("selected");
+          }
+        }
+        break;
+        case "page-4":
+        {
+
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup3[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup3[i]);
+            $(".selected").removeClass("selected");
+            $("#page-3").addClass("selected");
+          }
+        }
+        break;
+      }
+    }
+    break;
+    case "right-btn":
+    {
+      switch($(".selected").attr("id")){
+        case "page-1":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup2[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup2[i]);
+            $(".selected").removeClass("selected");
+            $("#page-2").addClass("selected");
+          }
+        }
+        break;
+        case "page-2":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup3[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup3[i]);
+            $(".selected").removeClass("selected");
+            $("#page-3").addClass("selected");
+          }
+        }
+        break;
+        case "page-3":
+        {
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup4[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup4[i]);
+            $(".selected").removeClass("selected");
+            $("#page-4").addClass("selected");
+          }
+        }
+        break;
+        case "page-4":
+        {
+
+          for(i = 0; i < $videoPicEle.length;i++){
+            $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup1[i] + ".jpg");
+            $($videoTitEle[i].childNodes[1]).text(videoGroup1[i]);
+            $(".selected").removeClass("selected");
+            $("#page-1").addClass("selected");
+          }
+        }
+        break;
+      }
+      break;
+    }
+  }
+});
+var nextPage = function(){
+  switch($(".selected").attr("id")){
+    case "page-1":
+    {
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup2[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup2[i]);
+        $(".selected").removeClass("selected");
+        $("#page-2").addClass("selected");
+      }
+    }
+    break;
+    case "page-2":
+    {
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup3[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup3[i]);
+        $(".selected").removeClass("selected");
+        $("#page-3").addClass("selected");
+      }
+    }
+    break;
+    case "page-3":
+    {
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup4[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup4[i]);
+        $(".selected").removeClass("selected");
+        $("#page-4").addClass("selected");
+      }
+    }
+    break;
+    case "page-4":
+    {
+
+      for(i = 0; i < $videoPicEle.length;i++){
+        $($videoPicEle[i].childNodes[1]).attr("src","./images/" + videoGroup1[i] + ".jpg");
+        $($videoTitEle[i].childNodes[1]).text(videoGroup1[i]);
+        $(".selected").removeClass("selected");
+        $("#page-1").addClass("selected");
       }
     }
     break;
   }
-});
+};
+
+setInterval(nextPage,6000);
